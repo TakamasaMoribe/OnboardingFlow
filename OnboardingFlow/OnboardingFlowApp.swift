9998ec00a0de83c4d16f861a9c79bda862657880
森部高昌 <takamasamoribe@gmail.com>
2026-04-29 11:35:43 +0900

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by 森部高昌 on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
